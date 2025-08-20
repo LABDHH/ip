b026717f3609c9d853d4ad90563be1e11cdfd9a6
Labdh <labdhnus@gmail.com>
2025-08-20 22:56:51 +0800

    Level 3. Mark as Done

--- src/main/java/Geni.java
+++ src/main/java/Geni.java
@@ -12,33 +12,71 @@ public class Geni {
         System.out.println(ui.getGreeting());
         Scanner sc = new Scanner(System.in);
         String inp = sc.nextLine().trim();
+        String[] inpt = inp.split(" ");
         ArrayList<Task> store = new ArrayList<>();
 
-        while (!inp.equals("bye")) {
+        while (!inpt[0].equals("bye")) {
+            if (inpt[0].equals("mark") || inpt[0].equals("unmark")) {
+                int i = Integer.parseInt(inpt[1]) - 1;
+                if (inpt[0].equals("mark")) {
 
-            if (inp.equals("list")) {
-                StringBuilder local_output = new StringBuilder();
-                int n = store.size();
-                local_output.append("____________________________________________________________\n");
-                for (int i = 0; i < n; i++) {
-                    local_output.append(i + 1).append(". ").append(store.get(i).getDescription()).append('\n');
+                    if (!store.get(i).getStatusIcon().equals("X")) {
+                        store.get(i).markAsDone();
+                    }
+                    StringBuilder local_output = new StringBuilder();
+                    local_output.append("____________________________________________________________\n");
+                    local_output.append("Nice! I've marked this task as done:\n");
+                    local_output.append("[")
+                            .append(store.get(i).getStatusIcon())
+                            .append("] ")
+                            .append(store.get(i).getDescription()).append('\n');
+                    local_output.append("____________________________________________________________\n");
+                    System.out.println(local_output);
+
                 }
-                local_output.append("____________________________________________________________\n");
-                System.out.println(local_output);
+                else {
+                    if (store.get(i).getStatusIcon().equals("X")) {
+                        store.get(i).markAsUndone();
+                    }
+                    StringBuilder local_output = new StringBuilder();
+                    local_output.append("____________________________________________________________\n");
+                    local_output.append("OK, I've marked this task as not done yet:\n");
+                    local_output.append("[")
+                                    .append(store.get(i).getStatusIcon())
+                                    .append("] ")
+                                    .append(store.get(i).getDescription()).append('\n');
+
+                    local_output.append("____________________________________________________________\n");
+                    System.out.println(local_output);
+                }
             }
             else {
-                Task task = new Task(inp);
-                store.add(task);
 
-                StringBuilder output = new StringBuilder();
+                if (inpt[0].equals("list")) {
+                    StringBuilder local_output = new StringBuilder();
+                    int n = store.size();
 
-                output.append("____________________________________________________________\n");
-                output.append("added: ").append(task.getDescription()).append('\n');
-                output.append("____________________________________________________________\n");
-                System.out.println(output);
+                    local_output.append("____________________________________________________________\n");
+                    for (int i = 0; i < n; i++) {
+                        local_output.append(i + 1).append(".").append("[").append(store.get(i).getStatusIcon()).append("] ").append(store.get(i).getDescription()).append('\n');
+                    }
+                    local_output.append("____________________________________________________________\n");
+                    System.out.println(local_output);
+                } else {
+                    Task task = new Task(inp);
+                    store.add(task);
 
+                    StringBuilder output = new StringBuilder();
+
+                    output.append("____________________________________________________________\n");
+                    output.append("added: ").append(task.getDescription()).append('\n');
+                    output.append("____________________________________________________________\n");
+                    System.out.println(output);
+
+                }
             }
             inp = sc.nextLine().trim();
+            inpt = inp.split(" ");
 
         }
 
