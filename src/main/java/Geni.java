93829e5ced6607855e811013bfeb172bd7552bbf
Labdh <labdhnus@gmail.com>
2025-08-20 16:44:02 +0800

    Level 1. Echo

--- src/main/java/Geni.java
+++ src/main/java/Geni.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Geni {
     private UI ui;
 
@@ -5,9 +7,21 @@ public class Geni {
         ui = new UI();
     }
     public void run() {
-        StringBuilder output = new StringBuilder();
-        output.append(ui.getGreeting()).append(ui.getExit());
-        System.out.println(output);
+
+        System.out.println(ui.getGreeting());
+        Scanner sc = new Scanner(System.in);
+        String inp = sc.nextLine().trim();
+        while (!inp.equals("bye")) {
+            StringBuilder output = new StringBuilder();
+            output.append("____________________________________________________________\n");
+            output.append(inp).append('\n');
+            output.append("____________________________________________________________\n");
+            System.out.println(output);
+            inp = sc.nextLine().trim();
+
+        }
+
+        System.out.println(ui.getExit());
     }
     public static void main(String[] args) {
         new Geni().run();
--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -4,12 +4,12 @@ public class UI {
         sb.append("____________________________________________________________\n");
         sb.append(" Hello! I'm Geni\n");
         sb.append(" What can I do for you?\n");
+        sb.append("____________________________________________________________\n");
         return sb.toString();
     }
 
     public String getExit() {
         StringBuilder sb = new StringBuilder();
-        sb.append("____________________________________________________________\n");
         sb.append(" Bye. Hope to see you again soon!\n");
         sb.append("____________________________________________________________");
         return sb.toString();
