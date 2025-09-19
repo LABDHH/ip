a94845140aea1f250a449d18b50972d461f2b9a5
Labdh <labdhnus@gmail.com>
2025-09-19 09:40:20 +0800

    Change the launcher of program

--- src/main/java/geni/Geni.java
+++ src/main/java/geni/Geni.java
@@ -78,8 +78,8 @@ public class Geni {
      *
      * @param args command-line arguments (not used)
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Launcher.main(args);
 
-    }
+    }*/
 }
